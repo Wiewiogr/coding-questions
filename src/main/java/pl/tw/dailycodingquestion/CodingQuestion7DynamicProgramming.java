ddf91fd7ad9e575605e3600af03a7c5174d5682f
Wiewiogr <wiewiora.tomasz1@gmail.com>
2019-11-03 10:41:51 +0100

    daily coding #7

---
+++ src/main/java/pl/tw/dailycodingquestion/CodingQuestion7DynamicProgramming.java
@@ -0,0 +1,44 @@
+package pl.tw.dailycodingquestion;
+
+public class CodingQuestion7DynamicProgramming {
+
+    public static void main(String[] args) {
+        String message = "01";
+        cache = new Integer[message.length()];
+        System.out.println(decode(message, 0));
+    }
+
+    private static Integer[] cache;
+
+    private static int decode(String message, int cursor) {
+
+        int remaining = message.length() - cursor;
+
+        if (remaining == 0) {
+            return 1;
+        }
+
+        if (message.charAt(cursor) == '0') {
+            return 0;
+        }
+
+        if (cache[cursor] != null) {
+            return cache[cursor];
+        }
+
+        int result;
+        if (remaining >= 2) {
+            Integer decoded = Integer.valueOf(message.substring(cursor, cursor + 2));
+            if (decoded <= 26 && decoded > 0) {
+                result = decode(message, cursor + 2) + decode(message, cursor + 1);
+            } else {
+                result = decode(message, cursor + 1);
+            }
+        } else {
+            result = decode(message, cursor + 1);
+        }
+
+        cache[cursor] = result;
+        return result;
+    }
+}
---
+++ src/main/java/pl/tw/dailycodingquestion/CodingQuestion7RecursiveNotOptimal.java
@@ -0,0 +1,51 @@
+package pl.tw.dailycodingquestion;
+
+import java.util.HashSet;
+
+/**
+ * This problem was asked by Facebook.
+ * <p>
+ * Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+ * <p>
+ * For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+ * <p>
+ * You can assume that the messages are decodable. For example, '001' is not allowed.
+ */
+public class CodingQuestion7RecursiveNotOptimal {
+
+    public static void main(String[] args) {
+        String message = "111";
+        decode(message, "", 0);
+        System.out.println(resultSet);
+    }
+
+    private static char[] mappings = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+
+    private static HashSet<String> resultSet = new HashSet<>();
+
+    private static void decode(String message, String result, int cursor) {
+
+
+        int remaining = message.length() - cursor;
+        if (remaining >= 1) {
+            String newResult = result + mappings[Integer.valueOf(message.charAt(cursor) + "") - 1];
+            if (remaining == 1) {
+                resultSet.add(newResult);
+            } else {
+                decode(message, newResult, cursor + 1);
+            }
+        }
+        if (remaining >= 2) {
+            Integer decoded = Integer.valueOf(message.substring(cursor, cursor + 2));
+            if (decoded < 26) {
+                String newResult = result + mappings[decoded - 1];
+                if (remaining == 2) {
+                    resultSet.add(newResult);
+                } else {
+                    decode(message, newResult, cursor + 2);
+                }
+            }
+        }
+    }
+}
