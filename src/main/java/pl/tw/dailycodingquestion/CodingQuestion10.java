ac629561295f2971552a941278bee12a11d47e02
Wiewiogr <wiewiora.tomasz1@gmail.com>
2019-11-06 18:57:56 +0100

    daily coding #10

---
+++ src/main/java/pl/tw/dailycodingquestion/CodingQuestion10.java
@@ -0,0 +1,23 @@
+package pl.tw.dailycodingquestion;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+ */
+
+public class CodingQuestion10 {
+
+    private static ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+    public static void main(String[] args) throws InterruptedException {
+        schedule(() -> System.out.println("dupa"), 100);
+        Thread.sleep(10000);
+    }
+
+    public static void schedule(Runnable job, int millis) {
+        executorService.schedule(job, millis, TimeUnit.MILLISECONDS);
+    }
+}
